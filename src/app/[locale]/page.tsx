c1e866a091421ebae45bc1c7e5dcd318e19c5eb6
sahrohit <sahrohit9586@gmail.com>
2024-05-27 20:38:52 +0545

    fix: :bug: updated home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,11 @@
-const Home = () => (
-  <div className="grid h-screen w-full place-items-center">Shubharambha</div>
-);
+import { useTranslations } from "next-intl";
+
+const Home = () => {
+  const t = useTranslations();
+
+  return (
+    <div className="grid h-screen w-full place-items-center">{t("apple")}</div>
+  );
+};
 
 export default Home;
