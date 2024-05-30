4063149689b2068c204a050f0f742ec6594f2f71
avintimilsina <avin.timilsina.16@gmail.com>
2024-05-31 00:36:35 +0545

    feat: :lipstick: added cuisine page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,12 @@
-import OurValuesSection from "@/components/sections/our-values-section";
+import Cuisine from "@/components/sections/about-cusine";
 import CardSection from "@/components/sections/card-section";
-        
+import OurValuesSection from "@/components/sections/our-values-section";
+
 const Home = () => (
   <>
     <OurValuesSection />
     <CardSection />
+    <Cuisine />
   </>
 );
 
