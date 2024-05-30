64665465fc8a7e33da8058b74ec21e8a801693e5
avintimilsina <avin.timilsina.16@gmail.com>
2024-05-30 23:22:35 +0545

    feat: :lipstick: added three image section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,8 @@
+import ThreeImageSection from "@/components/sections/three-image-section";
+
 const Home = () => (
-  <div className="grid h-screen w-full place-items-center">Shubharambha</div>
+  // <div className="grid h-screen w-full place-items-center">Shubharambha</div>
+  <ThreeImageSection />
 );
 
 export default Home;
