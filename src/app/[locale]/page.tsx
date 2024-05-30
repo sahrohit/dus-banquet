0fc6885fecd5b5219b5ffeeaf1af09a7df2fd55f
sahrohit <sahrohit9586@gmail.com>
2024-05-31 00:05:10 +0545

    feat(app): added carousel

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
-import OurValuesSection from "@/components/sections/our-values-section";
 import CardSection from "@/components/sections/card-section";
-        
+import HeroSection from "@/components/sections/hero-section";
+import OurValuesSection from "@/components/sections/our-values-section";
+
 const Home = () => (
   <>
+    <HeroSection />
     <OurValuesSection />
     <CardSection />
   </>
---
+++ src/config/hero.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/prefer-default-export */
+
+export const CAROUSEL = [
+  {
+    image: "/assets/value-img-1.jpeg",
+  },
+  {
+    image: "/assets/value-img-2.jpeg",
+  },
+  {
+    image: "/assets/value-img-3.jpeg",
+  },
+  {
+    image: "/assets/value-img-4.jpeg",
+  },
+  {
+    image: "/assets/value-img-5.jpeg",
+  },
+  {
+    image: "/assets/value-img-6.jpeg",
+  },
+];
