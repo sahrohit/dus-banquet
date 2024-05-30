3f11df8cc01782007832a2b0a236fe9c0d04e983
avintimilsina <avin.timilsina.16@gmail.com>
2024-05-30 21:54:58 +0545

    feat: :lipstick: added our values section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,8 @@
+import OurValuesSection from "@/components/sections/our-values-section";
+
 const Home = () => (
-  <div className="grid h-screen w-full place-items-center">Shubharambha</div>
+  // <div className="grid h-screen w-full place-items-center">Shubharambha</div>
+  <OurValuesSection />
 );
 
 export default Home;
